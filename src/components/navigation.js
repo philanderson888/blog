ff8d5f0608b4d353d1a4352a4a76a00dbe1400ba
philanderson888 <philanderson888@hotmail.com>
2019-10-26 16:55:52 +0100

    Adding VueJS to menu

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -2,15 +2,31 @@ import React from 'react'
 import Link from 'gatsby-link'
 import styles from './navigation.module.css'
 
-export default () => (
-  <nav role="navigation">
-    <ul className={styles.navigation}>
-      <li className={styles.navigationItem}>
-        <Link to="/">Home</Link>
-      </li>
-      <li className={styles.navigationItem}>
-        <Link to="/blog/">Blog</Link>
-      </li>
-    </ul>
-  </nav>
+export default () => ( <
+  nav role = "navigation" >
+  <
+  ul className = {
+    styles.navigation
+  } >
+  <
+  li className = {
+    styles.navigationItem
+  } >
+  <
+  Link to = "/" > Home < /Link> <
+  /li> <
+  li className = {
+    styles.navigationItem
+  } >
+  <
+  Link to = "/blog/" > Blog < /Link> <
+  /li> <
+  li > className = {
+    styles.navigationItem
+  } >
+  <
+  Link to = "https://epic-hamilton-f35937.netlify.com/" > VueJS Teaching Website < /Link> <
+  /li> <
+  /ul> <
+  /nav>
 )
