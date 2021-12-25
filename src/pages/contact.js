1abfc97a5c788b4d6f4e858b20a57ef6eb6e7ac0
philanderson888 <philanderson888@hotmail.com>
2021-12-25 01:47:44 +0000

    update contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -14,20 +14,34 @@ class ContactIndex extends React.Component {
       <div style={{ background: '#fff' }}>
         <Helmet title={siteTitle} />
         <div className={styles.hero}>
-          Blog
+          Contact Details
         </div>
         <div className="wrapper">
-          <h2 className="section-headline">Recent articles</h2>
-          <ul className="article-list">
-            {posts.map(({ node }) => {
-              return (
-                <li key={node.slug}>
-                  <ArticlePreview article={node} />
-                </li>
-              )
-            })}
+          <h2 className="section-headline">How To Find Me</h2>
+          <ul>
+            <li>
+              <em>Instagram</em><a href="https://www.instagram.com/philanderson888">philanderson888</a>
+            </li>
+            <li>
+              <em>LinkedIn</em><a href="https://www.linkedin.com/in/phil-anderson-60648a11/">phil-anderson-60648a11</a>
+            </li>
+            <li>
+              <em>GitHub</em><a href="https://github.com/philanderson888">philanderson888</a>
+              <ul>
+                <em>Flutter</em><a href="https://github.com/philanderson888/flutter">philanderson888/flutter</a>
+                <em>Java</em><a href="https://github.com/philanderson888/java-playground">philanderson888/java-playground</a>
+                <em>NodeJs</em><a href="https://github.com/philanderson888/nodejs">philanderson888/nodejs</a>
+                <em>Javascript</em><a href="https://github.com/philanderson888/javascript">philanderson888/javascript</a>
+                <em>Vue</em><a href="https://github.com/philanderson888/vue">philanderson888/vue</a>
+                <em>A+/Net+/Security+ Teaching Notes</em><a href="https://github.com/philanderson888/course-notes/tree/master/infrastructure">philanderson888/course-notes/infrastructure</a>
+              </ul>
+            </li>
           </ul>
         </div>
+        <div className={styles.hero}>
+          A Little About Me
+        </div>
+        <p>I am happily married, have been for many years now and live in North London with my wife and family.  My interests include playing the piano and guitar and working out regularly, also I am a passionate born-again Christian and help with charities such as helping people in <a href="https://www.gofundme.com/manage/christian-missionary-orphanage-in-pakistan">persecuted nations</a> and here at home in the UK also with volunteering with organisations such as <a href="https://northenfield.foodbank.org.uk">Enfield Food Bank</a> and <a href="https://jubileechurchlondon.org">Jubilee Church London</a></p>
       </div>
     )
   }
