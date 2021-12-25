9c430f96f192f414ae47e86a96de906d96020050
philanderson888 <philanderson888@hotmail.com>
2021-12-25 02:03:53 +0000

    updating contact details

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -51,7 +51,18 @@ class ContactIndex extends React.Component {
           A Little About Me
         </h2>
         <p>I am happily married, have been for many years now and live in North London with my wife and family.  My interests include playing the piano and guitar and working out regularly, also I am a passionate born-again Christian and help with charities such as helping people in <a href="https://www.gofundme.com/manage/christian-missionary-orphanage-in-pakistan">persecuted nations</a> and here at home in the UK also with volunteering with organisations such as <a href="https://northenfield.foodbank.org.uk">Enfield Food Bank</a> and <a href="https://jubileechurchlondon.org">Jubilee Church London</a></p>
+        <h2 className="section-headline">
+          Work And Career
+        </h2>
+        <p>
+          For many years I've been a tech trainer working with <a href="https://www.gov.uk/apply-apprenticeship">apprenticeships</a> but also as a contract trainer working freelance working with clients in the realm of IT Hardware, Software, Networking, Security and even CyberSecurity, delivering high quality training both in the UK and abroad to both civilian and military clients.
+        </p>
+        <p>
+          Recently I have turned my focus to being a programmer, initially focusing on training graduates into developer roles at the fantastic company <a href="https://www.spartaglobal.com">Sparta Global</a> but more recently I have gone to work myself as a software engineer.  I've been working with both <a href="https://reactjs.org">ReactJS</a> and more recently building TV applications with <a href="https://developer.roku.com/en-gb/overview">Roku Brightscript</a> and am currently enjoying working on a major contract for one of the UK's largest streaming TV providers.
+        </p>
+
       </div>
+
     )
   }
 }
