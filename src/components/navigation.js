bdd53143b4086a6973a309959453cf5e044a2461
philanderson888 <philanderson888@hotmail.com>
2022-01-25 04:41:20 +0000

    update navigation

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -14,11 +14,14 @@ export default () => (
   </li> 
   <li className = {
     styles.navigationItem
-  }><Link to = "/about/" > About < /Link> </li> 
+  }><Link to = "/about/" > About Me < /Link> </li> 
+  <li className = {
+    styles.navigationItem
+  }><Link to = "/about/" > About This Site < /Link> </li> 
   <li className = {
     styles.navigationItem
   }><Link to = "/blog/" > Blog < /Link> </li> 
-  <li className = {styles.navigationItem}><a href="https://epic-hamilton-f35937.netlify.com/">VueJS</a></li>
+  <li className = {styles.navigationItem}><a href="https://epic-hamilton-f35937.netlify.com/">VueJS with Bulma CSS</a></li>
   <li className = {
     styles.navigationItem
   }><Link to = "/contact/" > Contact < /Link> </li> 
