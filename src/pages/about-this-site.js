13403fbedca350c2f50ba126d095b00c4faf7091
philanderson888 <philanderson888@hotmail.com>
2022-01-25 05:03:34 +0000

    add space to rebuild site

--- src/pages/about-this-site.js
+++ src/pages/about-this-site.js
@@ -14,7 +14,7 @@ class AboutThisSiteIndex extends React.Component {
           
           <h2 className="section-headline">About This Site</h2>
           
-          <p>This site is built with Gatsby as a static site content generator website.  The blogs are stored in text format at `contentful.com` which stores the back-end content data and image links for the blog posts.  When the site is built, the data is fetched by gatsby and compiled together to produce a very fast static-site-generated website.  Enjoy!</p>
+          <p>This site is built with Gatsby as a static site content generator website.  The blogs are stored in text format at `contentful.com` which stores the back-end content data and image links for the blog posts.  When the site is built, the data is fetched by gatsby and compiled together to produce a very fast static-site-generated website.  Enjoy! </p>
           <br />
           <br />
           &copy; PhilAndersonWebsites 2020
