8ec38a91a6f4d9624a384211cecb7853d3dd6aec
philanderson888 <philanderson888@hotmail.com>
2022-07-27 06:54:37 +0100

    update bulma

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -22,7 +22,7 @@ export default () => (
     styles.navigationItem
   }><Link to = "/blog/" >Blog< /Link> </li> 
   <li className = {styles.navigationItem}><a href="https://epic-hamilton-f35937.netlify.com/">Teaching And Learning Tech</a></li>
-  <li className = {styles.navigationItem}><a href="https://epic-hamilton-f35937.netlify.com/">VueJS Site</a></li>
+  <li className = {styles.navigationItem}><a href="https://epic-hamilton-f35937.netlify.com/">VueJS Site With Bulma</a></li>
   <li className = {
     styles.navigationItem
   }><Link to = "/contact/" >Contact< /Link> </li> 
